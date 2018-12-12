bb3bbbef67626176f17b146f8436325bd45d2224
ycchen <ycc.tw.email@gmail.com>
2018-12-13 07:31:22 +0800

    [refactor] Move datagroup fixture into conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,30 @@
 import sys
 
 sys.path.insert(0, 'src')
+
+import pytest
+import pandas as pd
+
+from process import Datagroup
+
+
+@pytest.fixture
+def datagroup():
+    return Datagroup(ratings=pd.DataFrame({'userId': [1, 2, 3],
+                                           'movieId': [101, 102, 103],
+                                           'rating': [5, 4, 3],
+                                           'timestamp': [1112486027, 1112484727, 1094785740]}),
+                     tags=pd.DataFrame({'userId': [1, 2, 2],
+                                        'movieId': [101, 102, 103],
+                                        'tag': ['Mark Waters', 'New Zealand', 'surreal']}),
+                     movies=pd.DataFrame({'movieId': [101, 102, 103],
+                                          'title': ['A', 'B', 'C'],
+                                          'genres': [['Drama', 'Fantasy', 'Romance'],
+                                                     ['Drama', ],
+                                                     ['Fantasy', 'Romance']],
+                                          'year': [2010, 2014, 2009]}),
+                     genome=pd.DataFrame({'movieId': [101, 102, 103],
+                                          'tagId': [1001, 1001, 1001],
+                                          'relevance': [0.5, 0.4, 0.9],
+                                          'tag': ['yc', 'yc', 'yc']}),
+                     )
--- tests/test_process.py
+++ tests/test_process.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 from tempfile import TemporaryDirectory
 
-import pytest
 import pandas as pd
 
 import process
-from process import Datagroup
 
 
 def _dt2ts(dt):
@@ -50,28 +48,6 @@ def test_select_data_by_movie_group():
     expect_df = pd.DataFrame({'dummy': ['B', 'C', ], 'movieId': [2, 3, ]})
     df = process.select_data_by_movie_group(df, movie_group)
     pd.testing.assert_frame_equal(df, expect_df)
-
-
-@pytest.fixture
-def datagroup():
-    return Datagroup(ratings=pd.DataFrame({'userId': [1, 2, 3],
-                                           'movieId': [101, 102, 103],
-                                           'rating': [5, 4, 3],
-                                           'timestamp': [1112486027, 1112484727, 1094785740]}),
-                     tags=pd.DataFrame({'userId': [1, 2, 2],
-                                        'movieId': [101, 102, 103],
-                                        'tag': ['Mark Waters', 'New Zealand', 'surreal']}),
-                     movies=pd.DataFrame({'movieId': [101, 102, 103],
-                                          'title': ['A', 'B', 'C'],
-                                          'genres': [['Drama', 'Fantasy', 'Romance'],
-                                                     ['Drama', ],
-                                                     ['Fantasy', 'Romance']],
-                                          'year': [2010, 2014, 2009]}),
-                     genome=pd.DataFrame({'movieId': [101, 102, 103],
-                                          'tagId': [1001, 1001, 1001],
-                                          'relevance': [0.5, 0.4, 0.9],
-                                          'tag': ['yc', 'yc', 'yc']}),
-                     )
 
 
 def test_save_and_load_datagroup(datagroup):
