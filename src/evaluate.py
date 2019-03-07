ac6f5434b5e30cf801ebcb8fcd4a37b3335556a5
ycchen <ycc.tw.email@gmail.com>
2019-03-07 21:01:40 +0800

    [feature] Add RecommendEvaluator

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.metrics import average_precision_score
+
+import config
 
 
 class RatingEvaluator:
@@ -7,11 +10,11 @@ class RatingEvaluator:
         """Convert datagroup to the format for model fitting.
 
         Args:
-            user_movie_pair ({array-like, sparse matrix}, shape (n_samples, 2)):
+            user_movie_pair (numpy.array, shape (n_samples, 2)):
                 Pair of userId and movieId, where n_samples is the number of samples.
-            actu_y (array-like, shape (n_samples,)):
+            actu_y (numpy.array, shape (n_samples,)):
                 Actual target relative to user_movie_pair.
-            pred_y (array-like, shape (n_samples,)):
+            pred_y (numpy.array, shape (n_samples,)):
                 Predicted target relative to user_movie_pair.
 
         """
@@ -26,3 +29,40 @@ class RatingEvaluator:
 
         rms = (np.average(ele_square_diff))**0.5
         return rms
+
+
+class RecommendEvaluator:
+
+    def __init__(self, target, actions, recommendations, rec_scores):
+
+        self._target = target
+        self._actions = actions
+        self._recommendations = recommendations
+        self._rec_scores = rec_scores
+
+    def get_mean_average_precision(self, size=None):
+        if size is None:
+            size = self._recommendations.shape[1]
+
+        num_target = len(self._target)
+
+        selected_recs = self._recommendations[:, 0:size]
+        selected_rec_scores = self._rec_scores[:, 0:size]
+
+        true_scores = np.zeros(selected_rec_scores.shape, dtype='float64')
+        for i in range(num_target):
+            label = np.isin(selected_recs[i, :], self._actions[i])
+            true_scores[i, label] = 1
+
+        tot_ap = 0.0
+        for i in range(num_target):
+            y_true = true_scores[i, :]
+            y_score = selected_rec_scores[i, :]
+            if np.sum(y_true) < config.FLOAT_EPSILN:
+                ap = 0.0
+            else:
+                ap = average_precision_score(y_true, y_score)
+            tot_ap += ap
+        mean_ap = tot_ap / num_target
+
+        return mean_ap
