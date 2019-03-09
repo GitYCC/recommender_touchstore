eb2dacc4edc98065cc7472388646637722e70458
ycchen <ycc.tw.email@gmail.com>
2019-03-09 20:40:18 +0800

    [feature] Use flows

--- src/main.py
+++ src/main.py
@@ -2,18 +2,18 @@ import flow
 
 
 def main():
-    train_start_year = 1996
-    valid_start_year = 2010
+    train_start_year = 2011
+    valid_start_year = 2012
     convert_method = 'NoContentConverter'
     model_method = 'AverageModel'
-    evaluate_methods = ['rms']
+    topic = 'question2'
 
     datagroup_id = flow.prepare_datagroup(train_start_year, valid_start_year)
-    flow.train(datagroup_id, convert_method, model_method, evaluate_methods)
+    flow.train(datagroup_id, convert_method, model_method, topic)
 
-    flow.deploy(convert_method, model_method, evaluate_methods)
+    # flow.deploy(convert_method, model_method, topic)
 
-    flow.test_question1('d002a8e39a7046578a6a38e02962486a', model_method)
+    # flow.test('5095ac6b874743129f3bc38ce0c61e27')
 
 
 if __name__ == '__main__':
