979e23bd21a0f32183c8979cca59081f18dc7f79
ycchen <ycc.tw.email@gmail.com>
2019-04-22 16:14:19 +0800

    [feature] Change rec method from single to batch

--- src/models/model.py
+++ src/models/model.py
@@ -44,8 +44,9 @@ class BaseModel(ABC):
 
         """
 
-    def _recommend_for_one_user(self, user_id, movies, user_feature, movie_feature, maxsize):
-        user_movie_pair = np.array([[user_id, m] for m in movies], dtype='uint32')
+    def _recommend_for_users(self, users, movies, user_feature, movie_feature, maxsize):
+        user_movie_pair = np.array([[u, m] for m in movies for u in users], dtype='uint32')
+        user_index_mapping = {u: i for i, u in enumerate(users)}
 
         predicted = self.predict(user_movie_pair,
                                  user_feature=user_feature, movie_feature=movie_feature)
@@ -61,21 +62,25 @@ class BaseModel(ABC):
         df_table['rank'] = df_table['predicted'].rank(ascending=False, method='first')
         df_table = df_table[df_table['rank'] <= maxsize]
 
-        rec_items = np.full([1, maxsize], None, dtype='float64')
-        rec_scores = np.full([1, maxsize], None, dtype='float64')
+        rec_items = np.full([len(users), maxsize], None, dtype='float64')
+        rec_scores = np.full([len(users), maxsize], None, dtype='float64')
         for _, series in df_table.iterrows():
+            userId = series['userId']
             movieId = series['movieId']
             score = series['predicted']
             rank = series['rank']
 
+            index0 = int(user_index_mapping[userId])
             index1_rec_items = int(rank - 1)
-            rec_items[0, index1_rec_items] = movieId
-            rec_scores[0, index1_rec_items] = score
+            rec_items[index0, index1_rec_items] = movieId
+            rec_scores[index0, index1_rec_items] = score
 
         return (rec_items, rec_scores)
 
-    def _recommend_for_one_movie(self, movie_id, users, user_feature, movie_feature, maxsize):
-        user_movie_pair = np.array([[u, movie_id] for u in users], dtype='uint32')
+    def _recommend_for_movies(self, movies, users, user_feature, movie_feature, maxsize):
+        user_movie_pair = np.array([[u, m] for u in users for m in movies], dtype='uint32')
+        movie_index_mapping = {m: i for i, m in enumerate(movies)}
+
         predicted = self.predict(user_movie_pair,
                                  user_feature=user_feature, movie_feature=movie_feature)
 
@@ -90,16 +95,18 @@ class BaseModel(ABC):
         df_table['rank'] = df_table['predicted'].rank(ascending=False, method='first')
         df_table = df_table[df_table['rank'] <= maxsize]
 
-        rec_items = np.full([1, maxsize], None, dtype='float64')
-        rec_scores = np.full([1, maxsize], None, dtype='float64')
+        rec_items = np.full([len(movies), maxsize], None, dtype='float64')
+        rec_scores = np.full([len(movies), maxsize], None, dtype='float64')
         for _, series in df_table.iterrows():
+            movieId = series['movieId']
             userId = series['userId']
             score = series['predicted']
             rank = series['rank']
 
+            index0 = int(movie_index_mapping[movieId])
             index1_rec_items = int(rank - 1)
-            rec_items[0, index1_rec_items] = userId
-            rec_scores[0, index1_rec_items] = score
+            rec_items[index0, index1_rec_items] = userId
+            rec_scores[index0, index1_rec_items] = score
 
         return (rec_items, rec_scores)
 
@@ -117,6 +124,8 @@ class BaseModel(ABC):
                 Given more feature content about user.
             movie_feature (pandas.Dataframe, optional):
                 Given more feature content about movie.
+            maxsize (int):
+                Count of recommendation items
 
         Returns:
             recommended_items (array-like, shape (n_targets, n_recommended_items)):
@@ -125,30 +134,40 @@ class BaseModel(ABC):
         """
         rec_items, rec_scores = None, None
 
+        batch_num = 100
+
         if recommended_type == 'movie':
             print('recommend movies:')
-            for i in tqdm(range(len(users))):
-                sub_rec_items, sub_rec_scores = self._recommend_for_one_user(
-                    users[i], movies, user_feature, movie_feature, maxsize)
-                if rec_items is None and rec_scores is None:
-                    rec_items, rec_scores = sub_rec_items, sub_rec_scores
-                else:
-                    rec_items = np.vstack((rec_items, sub_rec_items))
-                    rec_scores = np.vstack((rec_scores, sub_rec_scores))
+            user_num = len(users)
+            with tqdm(total=user_num) as pbar:
+                for i in range(1+user_num//batch_num):
+                    start, end = i * batch_num, min((i+1) * batch_num, user_num)
+                    sub_rec_items, sub_rec_scores = self._recommend_for_users(
+                        users[start:end], movies, maxsize, user_feature, movie_feature)
+                    if rec_items is None and rec_scores is None:
+                        rec_items, rec_scores = sub_rec_items, sub_rec_scores
+                    else:
+                        rec_items = np.vstack((rec_items, sub_rec_items))
+                        rec_scores = np.vstack((rec_scores, sub_rec_scores))
+                    pbar.update(end - start)
 
             assert rec_items.shape[0] == len(users)
             assert rec_scores.shape[0] == len(users)
 
         elif recommended_type == 'user':
             print('recommend users:')
-            for i in tqdm(range(len(movies))):
-                sub_rec_items, sub_rec_scores = self._recommend_for_one_movie(
-                    movies[i], users, user_feature, movie_feature, maxsize)
-                if rec_items is None and rec_scores is None:
-                    rec_items, rec_scores = sub_rec_items, sub_rec_scores
-                else:
-                    rec_items = np.vstack((rec_items, sub_rec_items))
-                    rec_scores = np.vstack((rec_scores, sub_rec_scores))
+            movie_num = len(movies)
+            with tqdm(total=movie_num) as pbar:
+                for i in range(1+movie_num//batch_num):
+                    start, end = i * batch_num, min((i+1) * batch_num, movie_num)
+                    sub_rec_items, sub_rec_scores = self._recommend_for_one_movie(
+                        movies[start:end], users, maxsize, user_feature, movie_feature)
+                    if rec_items is None and rec_scores is None:
+                        rec_items, rec_scores = sub_rec_items, sub_rec_scores
+                    else:
+                        rec_items = np.vstack((rec_items, sub_rec_items))
+                        rec_scores = np.vstack((rec_scores, sub_rec_scores))
+                    pbar.update(end - start)
 
             assert rec_items.shape[0] == len(movies)
             assert rec_scores.shape[0] == len(movies)
