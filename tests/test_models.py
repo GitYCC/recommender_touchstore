53eb66efb1d16d1e817a93a6b0221a66ee554720
ycchen <ycc.tw.email@gmail.com>
2019-04-22 16:31:33 +0800

    [test] Change test due to changing testing data

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,7 +13,7 @@ class TestBaseModel:
         """MockModel implements BaseModel."""
 
         def fit(self, user_movie_pair, y,
-                user_feature=None, movie_feature=None):
+                user_feature=None, movie_feature=None, **model_params):
             pass
 
         def predict(self, user_movie_pair, user_feature=None, movie_feature=None):
@@ -36,7 +36,7 @@ class TestBaseModel:
 
     @pytest.mark.parametrize('recommended_type, maxsize', [('movie', None), ('user', None),
                                                            ('movie', 3), ('user', 3)])
-    def test_recommend(self, users, movies, recommended_type, maxsize):
+    def test_recommend__mock__right_order(self, users, movies, recommended_type, maxsize):
         model = self.MockModel()
         rec_items, rec_scores = model.recommend(recommended_type, users, movies, maxsize=maxsize)
 
@@ -60,36 +60,44 @@ class TestBaseModel:
 
 
 class TestPopularityModel:
+    case1_movie_popularity = {100: 3., 101: 1., 102: 3., 103: 2., 104: 4.5, 105: 5.}
 
-    def test_fix(self, user_movie_pair_1, ratings_1):
+    def test_fit__use_case1__right_rating_avg(self, user_movie_pair_1, ratings_1):
+        model = PopularityModel()
+
+        model = model.fit(user_movie_pair_1, ratings_1)
+
+        for movie in [100, 101, 102, 103, 104, 105]:
+            assert model._rating_avg[movie] == self.case1_movie_popularity[movie]
+
+    def test_predict__use_case1_predict_opp__right_ratings(
+            self, user_movie_pair_1, ratings_1, user_movie_pair_1_opp):
         model = PopularityModel()
         model.fit(user_movie_pair_1, ratings_1)
-        assert model._rating_avg[101] == 5.
-        assert model._rating_avg[102] == 1.5
-        assert model._rating_avg[103] == 3.5
 
-    def test_predict(self, user_movie_pair_1, ratings_1, user_movie_pair_2):
+        pred = model.predict(user_movie_pair_1_opp)
+
+        expected_pred = np.array(
+            [self.case1_movie_popularity[movie] for _, movie in user_movie_pair_1_opp.tolist()])
+        np.testing.assert_array_equal(pred, expected_pred)
+
+    def test_recommend__use_case1__right_items_and_scores(self, user_movie_pair_1, ratings_1):
         model = PopularityModel()
         model.fit(user_movie_pair_1, ratings_1)
-        pred = model.predict(user_movie_pair_2)
-        expect_pred = np.array([1.5, 5., 3.5, 5., np.nan])
-        np.testing.assert_array_equal(pred, expect_pred)
 
-    def test_recommend(self, user_movie_pair_1, ratings_1):
-        model = PopularityModel()
-        model.fit(user_movie_pair_1, ratings_1)
+        users = [0, 1, 2, 3, 4]
+        expected_movie_order = [105, 104, 102, 103, 101]
         rec_items, rec_scores = model.recommend(
-            recommended_type='movie', users=[1, 2, 3], movies=[101, 102, 103])
-        expected_rec_items = np.array([[101, 103, 102],
-                                       [101, 103, 102],
-                                       [101, 103, 102]])
-        expected_rec_scores = np.array([[5., 3.5, 1.5],
-                                        [5., 3.5, 1.5],
-                                        [5., 3.5, 1.5]])
+            recommended_type='movie',
+            users=users,
+            movies=sorted(expected_movie_order))
+
+        expected_rec_items = np.array([expected_movie_order for _ in range(len(users))])
+        expected_rec_scores = np.array([[5., 4.5, 3., 2., 1.] for _ in range(len(users))])
         np.testing.assert_array_equal(rec_items, expected_rec_items)
         np.testing.assert_array_equal(rec_scores, expected_rec_scores)
 
-    def test_save_and_load(self, tmpdir):
+    def test_save_and_load__do__restore(self, tmpdir):
         model = PopularityModel()
         model._rating_avg = {1: 0.2, 2: 0.5}
         model.save(tmpdir)
