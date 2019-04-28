c2daffcdadf2edfe1b370fb62594ede4d7b38e36
ycchen <ycc.tw.email@gmail.com>
2019-04-28 14:47:27 +0800

    [test] Downsize the FLOAT_EPSILN

--- src/config.py
+++ src/config.py
@@ -4,5 +4,4 @@ import os
 WORKSPACE = os.path.dirname(os.path.dirname(__file__))
 DIR_DATA = os.path.join(WORKSPACE, 'data')
 DIR_PRIVATE = os.path.join(WORKSPACE, 'prepare')
-
-FLOAT_EPSILN = 1.e-6
+FLOAT_EPSILN = 1.e-2
