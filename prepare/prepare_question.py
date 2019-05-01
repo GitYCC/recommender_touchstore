a3a8f3ba480a18f74a2d840f7e364c463d9ba3bd
ycchen <ycc.tw.email@gmail.com>
2019-05-01 13:32:06 +0800

    [feature] Prepare like problem question

--- prepare/prepare_question.py
+++ prepare/prepare_question.py
@@ -51,38 +51,29 @@ def prepare_question1():
 def prepare_question2():
     """Prepare Question 2.
 
-    Ranking Problem: We defined that the rating >= 3.0 as a favorite movie. Design a system to
+    Ranking Problem: We defined that the rating > 3.0 as a favorite movie. Design a system to
     recommend a top-10 favorite movies for a person, a `movieId` list which `userId` did not
     see before (provide `userId` at `./src/data/test_q2.txt`).
 
     """
     threshold_movies = 5
 
-    df_ratings_public = pd.read_csv(os.path.join(DIR_PUBLIC_DATA, 'ratings_pub.csv'))
-    df_ratings_private = pd.read_csv(os.path.join(WORKSPACE, 'ratings_prv.csv'))
+    df_likes_public = pd.read_csv(os.path.join(DIR_PUBLIC_DATA, 'likes_pub.csv'))
+    df_likes_private = pd.read_csv(os.path.join(WORKSPACE, 'likes_prv.csv'))
 
-    old_users = df_ratings_public['userId'].unique()
+    old_users = df_likes_public['userId'].unique()
     old_movies = _get_old_movies()
-    common = df_ratings_private.loc[:, ('userId', 'movieId')].merge(
-        df_ratings_public.loc[:, ('userId', 'movieId')], on=('userId', 'movieId'))
+    common = df_likes_private.loc[:, ('userId', 'movieId')].merge(
+        df_likes_public.loc[:, ('userId', 'movieId')], on=('userId', 'movieId'))
 
     selected = (
-        (df_ratings_private.rating >= 3.0) &
-        df_ratings_private.userId.isin(old_users) &
-        df_ratings_private.movieId.isin(old_movies) &
-        ((~df_ratings_private.userId.isin(common.userId)) &
-         (~df_ratings_private.movieId.isin(common.movieId)))
+        df_likes_private.userId.isin(old_users) &
+        df_likes_private.movieId.isin(old_movies) &
+        ((~df_likes_private.userId.isin(common.userId)) &
+         (~df_likes_private.movieId.isin(common.movieId)))
     )
-    df = df_ratings_private[selected]
-    df['info'] = df.loc[:, ('rating', 'movieId')].values.tolist()
-    dict_q2 = (
-        df
-        .groupby('userId')['info']
-        .apply(list)
-        .apply(lambda x: sorted(x, reverse=True) if len(x) >= threshold_movies else np.nan)
-        .dropna()
-        .to_dict()
-    )
+    df = df_likes_private[selected]
+    dict_q2 = df.groupby('userId')['movieId'].apply(list).dropna().to_dict()
 
     path_test = os.path.join(DIR_PUBLIC_DATA, 'test_q2.txt')
     path_answer = os.path.join(WORKSPACE, 'ans_q2.txt')
@@ -90,10 +81,11 @@ def prepare_question2():
     fw_test = open(path_test, 'w')
     fw_answer = open(path_answer, 'w')
 
-    for u, list_ in dict_q2.items():
+    for u, movies in dict_q2.items():
+        if len(movies) <= threshold_movies:
+            continue
         fw_test.write('{}\n'.format(int(u)))
-        movies = list(map(lambda x: str(int(x[1])), list_))
-        fw_answer.write('{}\n'.format(','.join(movies)))
+        fw_answer.write('{}\n'.format(','.join(map(str, movies))))
 
     fw_test.close()
     fw_answer.close()
@@ -102,7 +94,7 @@ def prepare_question2():
 def prepare_question3():
     """Prepare Question 3.
 
-    Content-based Problem: We defined that the rating >= 3.0 as a favorite movie.
+    Content-based Problem: We defined that the rating > 3.0 as a favorite movie.
     Design a system to recommend a top-10 `userId` they may like a new movie
     (at `./src/data/test_q3.txt`). We will give you some information of that new movie
     (at `./src/data/ref_movies_q3.csv` and `./src/data/ref_genome_q3.csv`).
@@ -110,31 +102,22 @@ def prepare_question3():
     """
     threshold_users = 5
 
-    df_ratings_public = pd.read_csv(os.path.join(DIR_PUBLIC_DATA, 'ratings_pub.csv'))
-    df_ratings_private = pd.read_csv(os.path.join(WORKSPACE, 'ratings_prv.csv'))
+    df_likes_public = pd.read_csv(os.path.join(DIR_PUBLIC_DATA, 'likes_pub.csv'))
+    df_likes_private = pd.read_csv(os.path.join(WORKSPACE, 'likes_prv.csv'))
 
-    old_users = df_ratings_public['userId'].unique()
+    old_users = df_likes_public['userId'].unique()
     new_movies = _get_new_movies()
-    common = df_ratings_private.loc[:, ('userId', 'movieId')].merge(
-        df_ratings_public.loc[:, ('userId', 'movieId')], on=('userId', 'movieId'))
+    common = df_likes_private.loc[:, ('userId', 'movieId')].merge(
+        df_likes_public.loc[:, ('userId', 'movieId')], on=('userId', 'movieId'))
 
     selected = (
-        (df_ratings_private.rating >= 3.0) &
-        df_ratings_private.userId.isin(old_users) &
-        df_ratings_private.movieId.isin(new_movies) &
-        ((~df_ratings_private.userId.isin(common.userId)) &
-         (~df_ratings_private.movieId.isin(common.movieId)))
+        df_likes_private.userId.isin(old_users) &
+        df_likes_private.movieId.isin(new_movies) &
+        ((~df_likes_private.userId.isin(common.userId)) &
+         (~df_likes_private.movieId.isin(common.movieId)))
     )
-    df = df_ratings_private[selected]
-    df['info'] = df.loc[:, ('rating', 'userId')].values.tolist()
-    dict_q3 = (
-        df
-        .groupby('movieId')['info']
-        .apply(list)
-        .apply(lambda x: sorted(x, reverse=True) if len(x) >= threshold_users else np.nan)
-        .dropna()
-        .to_dict()
-    )
+    df = df_likes_private[selected]
+    dict_q3 = df.groupby('movieId')['userId'].apply(list).dropna().to_dict()
 
     path_test = os.path.join(DIR_PUBLIC_DATA, 'test_q3.txt')
     path_answer = os.path.join(WORKSPACE, 'ans_q3.txt')
@@ -142,15 +125,16 @@ def prepare_question3():
     fw_test = open(path_test, 'w')
     fw_answer = open(path_answer, 'w')
 
-    for u, list_ in dict_q3.items():
-        fw_test.write('{}\n'.format(int(u)))
-        movies = list(map(lambda x: str(int(x[1])), list_))
-        fw_answer.write('{}\n'.format(','.join(movies)))
+    selected_movies = []
+    for m, users in dict_q3.items():
+        if len(users) <= threshold_users:
+            continue
+        selected_movies.append(m)
+        fw_test.write('{}\n'.format(int(m)))
+        fw_answer.write('{}\n'.format(','.join(map(str, users))))
 
     fw_test.close()
     fw_answer.close()
-
-    selected_movies = np.array(list(map(int, dict_q3.keys())))
 
     df_movies_private = pd.read_csv(os.path.join(WORKSPACE, 'movies_prv.csv'))
     df_genome_private = pd.read_csv(os.path.join(WORKSPACE, 'genome_prv.csv'))
